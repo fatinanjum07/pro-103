65de3e7198460bc4767d85f327a291ac0a4068b8
fatinanjum07 <113332116+fatinanjum07@users.noreply.github.com>
2023-10-24 21:34:46 +0600

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = “< Set path for tracking file system events>”
+
+#Event Handler Class
+Class FileEventHandler(FileSystemEventHandler):
+
+def on_created(self, event):
+    print(f"Hey, {event.src_path} has been created!")
+
+def on_deleted(self, event):
+    print(f"Oops! Someone deleted {event.src_path}!")
+
+# Initialize Event Handler Class
+# Initialize Observer
+# Schedule the Observer
+# Start the Observer
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+expect KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
